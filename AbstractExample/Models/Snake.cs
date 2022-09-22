47602b93ba1833b22d2ebd7cdad02f724a1c322f
nowakb27 <71862095+nowakb27@users.noreply.github.com>
2022-09-21 21:31:49 -0500

    Add files via upload
    Added Snake Class

---
+++ AbstractExample/Models/Snake.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AbstractExample.Models
+{
+    internal class Snake : Animal
+    {
+        public Snake(string name)
+        {
+            Name = name;
+        }
+
+        public Snake()
+        {
+            Name = "Fang";
+        }
+
+        public override void MakeNoise()
+        {
+            Console.WriteLine($"{Name} Hisses & Rattles");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine("hzzzzzzz");
+        }
+    }
+}
